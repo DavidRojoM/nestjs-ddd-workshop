a4319469fa9d0426d8b62a4794a2d59224f5406b
David Rojo <davidrojo1997@gmail.com>
2023-02-24 03:12:26 +0100

    fix: bind port to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { environment } from './environment/environment';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const port = environment.APP_PORT;
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(port);
+
+  Logger.log(`Application running on: http://localhost:${port}`);
 }
 bootstrap();
